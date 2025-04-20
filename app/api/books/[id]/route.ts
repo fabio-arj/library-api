a28290f5239001c9afff35e39006ae68ba8dc029
Fabio de Araujo <fabioaraujo1608@gmail.com>
2025-04-20 15:34:52 -0300

    Book endpoints

---
+++ app/api/books/[id]/route.ts
@@ -0,0 +1,103 @@
+import { PrismaClient } from "@/generated/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET({ params }: { params: Promise<{ id: number }> }) {
+  const { id } = await params;
+
+  if (!id) {
+    return NextResponse.json({ message: "Book ID not found" }, { status: 400 });
+  }
+
+  const book = await prisma.book.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+  return NextResponse.json(book, { status: 200 });
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: number }> }
+) {
+  try {
+    const { id } = await params;
+    const body = await req.json();
+    const {
+      title,
+      author,
+      published_year,
+      genre,
+      total_copies,
+      available_copies,
+    } = body;
+
+    if (!id) {
+      return NextResponse.json(
+        { message: "User ID not found" },
+        { status: 400 }
+      );
+    }
+
+    if (
+      !title ||
+      !author ||
+      !published_year ||
+      !genre ||
+      !total_copies ||
+      !available_copies
+    ) {
+      return NextResponse.json(
+        { message: "Empty mandatory fields" },
+        { status: 400 }
+      );
+    }
+    await prisma.book.update({
+      where: {
+        id: id,
+      },
+      data: {
+        title: title,
+        author: author,
+        published_year: published_year,
+        genre: genre,
+        total_copies: total_copies,
+        available_copies: available_copies,
+      },
+    });
+
+    return NextResponse.json("Book data updated!", { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error updating book data", error },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE({ params }: { params: Promise<{ id: number }> }) {
+  try {
+    const { id } = await params;
+
+    if (!id) {
+      return NextResponse.json(
+        { message: "Book ID not found" },
+        { status: 400 }
+      );
+    }
+
+    await prisma.book.delete({
+      where: {
+        id: id,
+      },
+    });
+    return NextResponse.json({ message: "Book deleted!" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error deleting book", error },
+      { status: 500 }
+    );
+  }
+}
--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@/generated/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
 export async function GET() {
   try {
     const books = await prisma.book.findMany();
-    return NextResponse.json(books);
+    return Response.json(books, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { message: "Error getting books", error },
@@ -15,6 +15,46 @@ export async function GET() {
   }
 }
 
-export async function POST() {
-  //TODO
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const {
+      title,
+      author,
+      published_year,
+      genre,
+      total_copies,
+      available_copies,
+    } = body;
+
+    await prisma.book.create({
+      data: {
+        title: title,
+        author: author,
+        published_year: published_year,
+        genre: genre,
+        total_copies: total_copies,
+        available_copies: available_copies,
+      },
+    });
+
+    return NextResponse.json("Book added!", { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error adding book", error },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE() {
+  try {
+    await prisma.book.deleteMany();
+    return NextResponse.json("Books deleted!", { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error deleting books", error },
+      { status: 500 }
+    );
+  }
 }
